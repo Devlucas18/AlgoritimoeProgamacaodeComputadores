e4e10f892c237d03b844ed130e1dc3d79d9ab704
Lucas Araujo <lucasaraujo.doc@gmail.com>
2025-08-20 20:34:11 -0300

    Primeirocodigo

---
+++ escrevercabecalho.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Disciplina: Algoritimos e Progamação Estruturada");
+    printf("Turma: GPE17N20241");
+    printf("Nome: Lucas de Souza Martins Araújo");
+    printf("Matricula: UC25103665");
+    return 0;
+}
