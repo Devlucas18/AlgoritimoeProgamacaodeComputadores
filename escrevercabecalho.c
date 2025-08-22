6090ddf6b2e9750b0b573037dc55b09754c5778f
Lucas Araujo <lucasaraujo.doc@gmail.com>
2025-08-22 08:38:29 -0300

    ExercicioScanf

--- escrevercabecalho.c
+++ escrevercabecalho.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Disciplina: Algoritimos e Progamação Estruturada");
-    printf("Turma: GPE17N20241");
-    printf("Nome: Lucas de Souza Martins Araújo");
-    printf("Matricula: UC25103665");
+    char disciplina[250];
+    char turma[12];
+    char nome[100];
+    char matricula[12];
+    printf("Digite a disciplina: ");
+    scanf("%s", disciplina);
+
+    printf("Digite a turma: ");
+    scanf("%s", turma);
+
+    printf("Digite o nome: ");
+    scanf("%s", nome);
+
+    printf("Digite a matricula: ");
+    scanf("%s", matricula);
+
+    printf("Disciplina: %s\n", disciplina);
+    printf("Turma: %s\n", turma);
+    printf("Nome: %s\n", nome);
+    printf("Matricula: %s\n", matricula);
     return 0;
 }
