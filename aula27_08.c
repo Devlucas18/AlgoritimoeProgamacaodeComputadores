9b3fb7df44d51c9532ed746970a5184055df93c9
Lucas Araujo <lucasaraujo.doc@gmail.com>
2025-08-27 21:52:08 -0300

    atualizado27_08

--- aula27_08.c
+++ aula27_08.c
@@ -19,11 +19,18 @@ int main(){
     printf("Nome: %sIdade: %d anos\nMassa corporal: %f kg\nAltura: %f m\n", nome, idade, kg, m);
     //calcular e exibir o IMC
     // massa/altura^2
-    if (altura > 0){
-        float imc = kg/ (m*m);
-        printf("%f", imc);
-    }
+    float imc = kg/ (m*m);
     
+    if (imc < 18.5){
+        puts("Abaixo do peso");
+    } else if (imc >= 18.5 && imc < 24.9){
+        puts("Peso normal");
+    } else if (imc >= 25 && imc < 29.9){
+        puts("Sobrepeso");
+    } else if (imc >= 30 && imc < 34.9){
+        puts("Obesidade");
+    }
+    printf("%f", imc);
     return 0;
 
 
